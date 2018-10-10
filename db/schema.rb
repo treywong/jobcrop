3f9235c5913a21bca81676e3dbc248eb78fc18f5
qwerqy <amnrsln@gmail.com>
2018-10-10 21:31:40 +0800

    hotfixes

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,6 @@ ActiveRecord::Schema.define(version: 2018_10_10_090454) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "bookings", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "job_id"
