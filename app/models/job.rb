2bc67ff1de777c16341f1e5bda9d4602922f8685
qwerqy <amnrsln@gmail.com>
2018-10-17 22:13:18 +0800

    bug fixes

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,7 +3,7 @@ class Job < ApplicationRecord
 	has_many :bookings, dependent: :destroy
 
 	include PgSearch
-	pg_search_scope :search_by_title, :against => [:title, :location], :using => {:tsearch => {:any_word => true}}
+	pg_search_scope :search_by_title, :against => [:title], :using => {:tsearch => {:any_word => true}}
 
 	def location
     	"#{self.address} #{self.state} #{self.country} #{self.zipcode}"
