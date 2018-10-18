e40ad42c07e94fbd5a8be7aedc485a0e5c3192ac
qwerqy <amnrsln@gmail.com>
2018-10-18 15:20:59 +0800

    more big fix

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,13 +1,18 @@
 class FollowsController < ApplicationController
   def create
-    @jobhunter = current_user.jobhunter
-    @company = Company.find(params[:company_id])
-    @follow = @company.follows.build(jobhunter_id: @jobhunter.id)
-    if @follow.save
-      redirect_back(fallback_location: @company)
+    if current_user.profile_complete?
+      @jobhunter = current_user.jobhunter
+      @company = Company.find(params[:company_id])
+      @follow = @company.follows.build(jobhunter_id: @jobhunter.id)
+      if @follow.save
+        redirect_back(fallback_location: @company)
+      else
+        flash[:danger] = "Something's wrong. #{@follow.errors.full_messages.to_sentence}"
+        redirect_back(fallback_location: @company)
+      end
     else
-      flash[:danger] = "Something's wrong. #{@follow.errors.full_messages.to_sentence}"
-      redirect_back(fallback_location: @company)
+      flash[:warning] = "Please complete profile before applying for any job."
+      redirect_to user_path(current_user.id)
     end
   end
 
